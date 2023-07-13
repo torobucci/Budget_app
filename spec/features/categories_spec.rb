1996ad741346b1e68689bb93141fdf7e982eacd5
torobucci <kevintoro254@gmail.com>
2023-07-13 15:52:58 +0300

    update feature specs

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Categories', type: :feature do
   let!(:user) { User.create!(name: 'john_doe', email: 'john5@example.com', password: '123456') }
-  let!(:category) { Category.create!(name: 'Food', icon: 'meal', author: user) }
-  let!(:category1) { Category.create!(name: 'Clothes', icon: 'gucci', author: user) }
-  let!(:category2) { Category.create!(name: 'Travel', icon: 'Turkish airlines', author: user) }
+  let!(:category) { Category.create!(name: 'Food', icon: 'https://previews.123rf.com/images/olgastrelnikova/olgastrelnikova1901/olgastrelnikova190100001/115903194-food-icon-with-smile-label-for-food-company-grocery-store-icon-vector-illustration-with-smiling.jpg', author: user) }
+  let!(:category1) { Category.create!(name: 'Clothes', icon: 'https://previews.123rf.com/images/olgastrelnikova/olgastrelnikova1901/olgastrelnikova190100001/115903194-food-icon-with-smile-label-for-food-company-grocery-store-icon-vector-illustration-with-smiling.jpg', author: user) }
+  let!(:category2) { Category.create!(name: 'Travel', icon: 'https://previews.123rf.com/images/olgastrelnikova/olgastrelnikova1901/olgastrelnikova190100001/115903194-food-icon-with-smile-label-for-food-company-grocery-store-icon-vector-illustration-with-smiling.jpg', author: user) }
 
   before do
     visit new_user_session_path
@@ -19,7 +19,21 @@ RSpec.describe 'Categories', type: :feature do
     it 'displays a list of categories' do
       visit categories_path
 
-      expect(page).to have_content('LOGIN')
+      expect(page).to have_content(category.name)
+      expect(page).to have_content(category1.name)
+      expect(page).to have_content(category2.name)
+      expect(page).to have_css('.category_amount', text: "$#{category.total_payments}")
+    end
+
+    describe 'New page' do
+      it 'creates a new category' do
+        visit new_category_path
+
+        expect(page).to have_css('.payments-header')
+        expect(page).to have_css('.payments-header i.fa-bars')
+        expect(page).to have_css('.payments-header h2', text: 'ADD NEW CATEGORY')
+        expect(page).to have_link(href: categories_path) { find('i.fa-arrow-left') }
+      end
     end
   end
 end
--- spec/features/payments_spec.rb
+++ spec/features/payments_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Payments', type: :feature do
   let!(:user) { User.create!(name: 'john_doe', email: 'john8@example.com', password: '123456') }
-  let!(:category) { Category.create!(name: 'Food', icon: 'meal', author: user) }
+  let!(:category) { Category.create!(name: 'Food', icon: 'https://previews.123rf.com/images/olgastrelnikova/olgastrelnikova1901/olgastrelnikova190100001/115903194-food-icon-with-smile-label-for-food-company-grocery-store-icon-vector-illustration-with-smiling.jpg', author: user) }
   let!(:payment1) { category.payments.create!(name: 'Payment 1', amount: 10, author: user) }
   let!(:payment2) { category.payments.create!(name: 'Payment 2', amount: 20, author: user) }
   let!(:payment3) { category.payments.create!(name: 'Payment 3', amount: 30, author: user) }
-  let(:payments) { [payment1, payment2, payment3] }
 
   before do
     visit new_user_session_path
@@ -19,7 +18,22 @@ RSpec.describe 'Payments', type: :feature do
     it 'displays the payments' do
       visit category_payments_path(category)
 
-      expect(page).to have_content('LOGIN')
+      expect(page).to have_content(payment1.name)
+      expect(page).to have_content(payment2.name)
+      expect(page).to have_content(payment3.name)
+      expect(page).to have_css('.payments-header')
+      expect(page).to have_link('Add new payment', href: new_category_payment_path(category), class: 'add-new-link')
+    end
+  end
+
+  describe 'New page' do
+    it 'creates a new category' do
+      visit new_category_payment_path(category)
+
+      expect(page).to have_css('.payments-header')
+      expect(page).to have_css('.payments-header i.fa-bars')
+      expect(page).to have_css('.payments-header h2', text: 'ADD NEW PAYMENT')
+      expect(page).to have_link(href: category_payments_path(category)) { find('i.fa-arrow-left') }
     end
   end
 end
